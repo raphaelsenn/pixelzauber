6ff4b0521ec3e45bf6127c306156eea6fa2234af
raphaelsenn <raphaelsenn@outlook.de>
2025-05-21 18:36:00 +0200

    update readme

--- main.cpp
+++ main.cpp
@@ -17,12 +17,12 @@ int main() {
   lena.readPGM("lena.pgm");
   printf("Opened lena.pgm\n");
 
-  // Mat2d<int> kernel = Mat2d<int>({{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}});
-  Mat2d<int> kernel = Mat2d<int>({{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}});
+  Mat2d<int> kernel = Mat2d<int>({{0, -1, 0}, {-1, 5, -1}, {0, -1, 0}});
+  // Mat2d<int> kernel = Mat2d<int>({{-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}});
   Mat2d<int> lena_edges = applyFilter(lena, kernel);
   printf("Applied filter to lena.pgm\n");
   Mat2d<int> res(lena_edges);
-  Mat2d<int> res2 = res.clip(0, 245);
+  Mat2d<int> res2 = res.clip(0, res.maxVal());
   
   res2.writePGM("lena_mod.pgm");
   printf("Written lena_mod.pgm\n");
